938d233e6c57e1d38033d9106239fe35757d1b68
him <goyalhami@gmail.com>
2017-11-13 12:17:36 +0530

    First commit

---
+++ src/main/java/com/cg/App.java
@@ -0,0 +1,16 @@
+package com.cg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	try{
+	int i;
+	System.out.println( "Hello World!" );
+    }catch(Exception e){}
+}
+}
